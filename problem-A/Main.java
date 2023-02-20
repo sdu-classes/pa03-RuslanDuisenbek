86470ff0cf3ed3f89ac047b75708dd16a5fa85c5
RuslanDuisenbek <124431912+RuslanDuisenbek@users.noreply.github.com>
2023-02-20 19:32:24 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -1,10 +1,47 @@
-package problemCalculator;
+
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);package problemCalculator;
 
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Calculator c = null;
+        try{
+            c = new Calculator(scan.nextInt(), scan.nextInt());
+        }catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+       try{
+           c.Add();
+       }catch (ArithmeticException e){
+           System.out.println(e.getMessage());
+       }
+       try{
+           c.Subtract();
+       }catch (ArithmeticException e){
+           System.out.println(e.getMessage());
+       }
+       try{
+           c.Multiplication();
+       }catch (ArithmeticException e){
+           System.out.println(e.getMessage());
+       }
+       try {
+           c.Division();
+       }catch (ArithmeticException e){
+           System.out.println(e.getMessage());
+       }
+    }
+}
+
         Calculator c = new Calculator(scan.nextInt(), scan.nextInt());
        try{
            c.Add();
