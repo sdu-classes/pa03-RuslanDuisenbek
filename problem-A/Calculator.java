e17b6cfb041b71b64882a55c8d518eba0430b34d
RuslanDuisenbek <124431912+RuslanDuisenbek@users.noreply.github.com>
2023-02-20 19:33:18 +0600

    Update Calculator.java

--- problem-A/Calculator.java
+++ problem-A/Calculator.java
@@ -1,11 +1,15 @@
-package problemCalculator;
 
-public class Calculator {
-    public int n ;
-    public int m;
-    public Calculator(int n, int m){
+
+public class Calculator  {
+    private int n ;
+    private int m;
+    public Calculator(int n, int m) {
         this.n = n;
         this.m = m;
+    }
+    public Calculator(Object o1 , Object o2){
+        if(o1 instanceof Integer)
+            throw new NumberFormatException("This is not Integer");
     }
     public  void Add(){
         if(this.n < 0 || this.m < 0) throw new ArithmeticException(String.format("One of these numbers is negative : %d ,%d ",this.n , this.m));
@@ -18,12 +22,12 @@ public class Calculator {
     }
     public void Multiplication(){
         if(this.n == 0 || this.m == 0)
-            throw new ArithmeticException(String.format("One of these numbers are zero : %d , %d" , this.n , this.m));
+            throw new ArithmeticException(String.format("One of these numbers is zero : %d , %d" , this.n , this.m));
         System.out.println(this. n * this.m);
     }
     public void Division(){
         if(this.n == 0 || this.m == 0)
-            throw new ArithmeticException(String.format("One of these numbers are zero : %d , %d" , this.n , this.m));
+            throw new ArithmeticException(String.format("One of these numbers is zero : %d , %d" , this.n , this.m));
         System.out.println(this.n / this.m);
     }
 }
